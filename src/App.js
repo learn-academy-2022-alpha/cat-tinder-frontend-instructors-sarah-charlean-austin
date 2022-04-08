326acdceee47886fbd60b8d065c15c4c03b2a6c4
WalkerTexas-coder <austinsrwalker@gmail.com>
2022-04-08 14:13:22 -0700

    fetch for READ, CREATE, and UPDATE

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import CatShow from './pages/CatShow'
 import CatNew from './pages/CatNew'
 import CatEdit from './pages/CatEdit'
 import NotFound from './pages/NotFound'
-import mockCats from './mockCats'
 import {
   BrowserRouter as Router,
   Route,
@@ -19,15 +18,60 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      cats: mockCats
+      cats: []
     }
   }
 
+  componentDidMount(){
+    this.readCat()
+  }
+
+// CRUD actions for cats
   createCat = (newCat) => {
-    console.log("Cat was created", newCat)
+    //fetch(argument1, argument2)
+    fetch("http://localhost:3000/cats", {
+      // converting an object to a string
+      body: JSON.stringify(newCat),
+      // specify the info being sent in JSON and the info returning should be JSON
+      headers: {
+        "Content-Type": "application/json"
+      },
+      // HTTP verb so the correct endpoint is invoked on the server
+      method: "POST"
+    })
+      .then(response => response.json())
+      .then(payload => this.readCat())
+  }
+
+  readCat = () => {
+    // fetch should hit cats#index on the rails backend
+    fetch("http://localhost:3000/cats")
+    .then(response => response.json())
+    .then(payload => this.setState({ cats: payload }))
+    .catch(errors => console.log("Cat Read Errors", errors))
+  }
+
+  updateCat = (updatedCat, id) => {
+    fetch(`http://localhost:3000/cats/${id}`, {
+      // converting an object to a string
+      body: JSON.stringify(updatedCat),
+      // specify the info being sent in JSON and the info returning should be JSON
+      headers: {
+        "Content-Type": "application/json"
+      },
+      // HTTP verb so the correct endpoint is invoked on the server
+      method: "PATCH"
+    })
+      .then(response => response.json())
+      .then(payload => this.readCat())
+  }
+
+  deleteCat = (id) => {
+    console.log(`Cat at ${id} was deleted`, id)
   }
 
   render() {
+    console.log(this.state)
     return(
       <Router>
         <Header />
